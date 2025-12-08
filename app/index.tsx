b882302df31fdcd0c342a5ca79092586f42f2306
Nehuén Cabibbo <nehuen.cabibbo@mercadolibre.com>
2025-12-08 00:38:35 -0300

    feat: Add Prettier configuration, update Babel presets, and enhance component structure with new TaskCard and Checkbox components

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,20 @@
-import { View } from "react-native";
+import { useColorScheme, View } from "react-native";
 import { Button } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
+import Entypo from "@expo/vector-icons/Entypo";
+import TaskCard from "@/components/TaskCard";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Button className="w-10 bg-red-400">
-        <Text className="text-blue-500">aaaaaa</Text>
+    <View className="p-6 flex-1 items-center justify-center">
+      <TaskCard title="Task 1" description="Description 1" />
+      <TaskCard title="Task 2" description="Description 2" />
+      <TaskCard title="Task 3" description="Description 3" />
+
+      <Text className="mt-4">Edit app/index.tsx to edit this screen.</Text>
+      <Button className="absolute bottom-6 right-6 w-14 h-14 flex items-center justify-center p-0">
+        <Entypo name="plus" size={30} color="white" />
       </Button>
-      <Text>ss</Text>
-      <Text style={{ marginTop: 12 }}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
---
+++ components/TaskCard.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { cn } from "@/lib/utils";
+import { View } from "react-native";
+import { Checkbox } from "./ui/checkbox";
+import { useState } from "react";
+
+interface TaskCardProps {
+  title: string;
+  description: string;
+}
+
+export default function TaskCard({ title, description }: TaskCardProps) {
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <Button className="flex shadow-md w-full min-h-24 gap-5">
+      <Checkbox
+        className="w-7 h-7 rounded-full border-2 border-neutral-400"
+        checkedClassName="bg-white"
+        indicatorClassName={cn(
+          "rounded-full bg-white transition-all duration-200 ease-out",
+          checked ? "scale-90 opacity-100" : "scale-0 opacity-0"
+        )}
+        iconClassName="text-black"
+        checked={checked}
+        onCheckedChange={setChecked}
+      />
+      <View className="flex-col justify-start flex-1">
+        <Text className="text-lg font-bold">{title}</Text>
+        <Text className="opacity-80">{description}</Text>
+      </View>
+      <View className="rounded-full w-4 h-4 bg-red-500"/>
+    </Button>
+  );
+}
